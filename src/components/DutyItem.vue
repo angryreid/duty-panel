74ea43e8dfbadfd07950941ea6261a6aecf3eda3
angryreid <dxiang1391458323@foxmail.com>
2019-02-19 16:14:07 +0800

    update

---
+++ src/components/DutyItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h6 v-show="list.length">{{day}}</h6>
+    <div v-for="item in list">
+      <Tag :type="type" :color="color">{{item}}</Tag>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DutyItem",
+  props: {
+    day: {
+      type: String,
+      default: ""
+    },
+    list: {
+      type: Array,
+      default: () => []
+    },
+    type: {
+      type: String,
+      default: ""
+    },
+    color: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    showDutyItem(item) {
+      console.log(item);
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
+
+
--- src/components/DutyPanel.vue
+++ src/components/DutyPanel.vue
@@ -17,7 +17,12 @@
           <Option :value="1">本周</Option>
           <Option :value="2">单双周</Option>
         </Select>
-        <DatePicker type="month" placeholder="please select month..." v-show="type == 2" :value="month"></DatePicker>&nbsp;&nbsp;
+        <DatePicker
+          type="month"
+          placeholder="please select month..."
+          v-show="type == 2"
+          v-model="month"
+        ></DatePicker>&nbsp;&nbsp;
         <Button type="primary" @click.native="serchData">查询</Button>
       </Col>
     </Row>
@@ -26,8 +31,7 @@
       <Col span="24">
         <Card>
           <p slot="title">
-            <Icon type="ios-film-outline"></Icon>
-            本 周 :
+            <Icon type="ios-film-outline"></Icon>本 周 :
           </p>
           <Table :columns="weekColumns" :data="weekData" class="mb"></Table>
         </Card>
@@ -38,50 +42,58 @@
       <Col span="24">
         <Card>
           <p slot="title">
-            <Icon type="ios-film-outline"></Icon>
-            单 周 :
+            <Icon type="ios-film-outline"></Icon>单 周 :
           </p>
-          <Table :columns="weekColumns" :data="weekData" class="mb"></Table>
+          <Table :columns="weekColumns" :border="true" :data="weekData" class="mb"></Table>
         </Card>
         <Card>
           <p slot="title">
-            <Icon type="ios-film-outline"></Icon>
-            双 周 :
+            <Icon type="ios-film-outline"></Icon>双 周 :
           </p>
-          <Table :columns="weekColumns" :data="weekData" class="mb"></Table>
+          <Table :columns="weekColumns" :border="true" :data="weekData" class="mb"></Table>
         </Card>
       </Col>
     </Row>
     <!-- 月份 -->
     <Row class="mb p20" v-show="type == 2">
       <Col span="24">
-        <Table :columns="monthColumns" :data="monthData" ></Table>
+        <Table :columns="monthColumns" :border="true" :data="monthData"></Table>
       </Col>
     </Row>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import DutyItem from "./DutyItem";
+Vue.component(DutyItem.name, DutyItem);
 export default {
   name: "DutyPanel",
   props: {},
+  components: {
+    // DutyItem
+  },
   data() {
     return {
       theme: "light",
-      type: 1,
+      type: 2,
       week: 1,
       month: new Date(),
+      dutyList: ["1", "2"],
       weekColumns: [
         {
           title: "Name",
-          key: "name"
+          key: "name",
+          align: "center"
         },
         {
           title: "Age",
-          key: "age"
+          key: "age",
+          align: "center"
         },
         {
           title: "Address",
-          key: "address"
+          key: "address",
+          align: "center"
         }
       ],
       weekData: [
@@ -110,49 +122,119 @@ export default {
           date: "2016-10-04"
         }
       ],
-      monthColumns:[
+      monthColumns: [
         {
           title: "班次",
-          key: "name"
+          key: "shift",
+          align: "center"
         },
         {
           title: "周一",
-          key: "name"
+          key: "mon",
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "DutyItem",
+                {
+                  props: {
+                    day: "1",
+                    list: params.row.mon,
+                    type: "dot",
+                    color: "primary"
+                  },
+                  style: {},
+                  on: {
+                    click: () => {
+                      console.log(params.row.mon);
+                    }
+                  }
+                },
+                params.row.mon
+              )
+            ]);
+          }
         },
         {
           title: "周二",
-          key: "name"
+          key: "tues",
+          align: "center"
         },
         {
           title: "周三",
-          key: "name"
+          key: "wed",
+          align: "center"
         },
         {
           title: "周四",
-          key: "name"
+          key: "thur",
+          align: "center"
         },
         {
           title: "周五",
-          key: "name"
+          key: "fri",
+          align: "center"
         },
         {
           title: "周六",
-          key: "name"
+          key: "sat",
+          align: "center"
         },
         {
           title: "周日",
-          key: "name"
-        },
+          key: "sun",
+          align: "center"
+        }
       ],
       monthData: []
     };
   },
-  created(){
-    console.log(this.month)
+  created() {
+    this.getTableData();
   },
   methods: {
-    serchData(){
-      console.log('search')
+    serchData() {
+      console.log("search...");
+      this.getTableData();
+    },
+    // 返回某月共有多少天
+    getDayNumber(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+    // 返回某个月第一天是周几
+    getFirstDayWeek(year, month) {
+      let week = new Date(year, month, 1).getDay();
+      return week == 0 ? 7 : week;
+    },
+    getTableData() {
+      let dayNum = this.getDayNumber(
+          this.month.getFullYear(),
+          this.month.getMonth() + 1
+        ),
+        dutyArr = [],
+        firstOfWeek = this.getFirstDayWeek(
+          this.month.getFullYear(),
+          this.month.getMonth() + 1
+        );
+      let weekObj = {
+        mon: "0",
+        tues: "1",
+        wed: "2",
+        thur: "3",
+        fri: "4",
+        sat: "5",
+        sun: "6"
+      };
+      dutyArr[0] = {
+        shift: "班组1",
+        mon: [1, 2],
+        tues: ["班组1", "班组2"],
+        wed: "3",
+        thur: "4",
+        fri: "5",
+        sat: "6",
+        sun: "7"
+      };
     }
   }
 };
